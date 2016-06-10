ce74863a71bc63c079b41596bf731ebc87ea8d8a
Collin Smith <collinsmith70@gmail.com>
2016-06-10 00:34:49 -0700

    Added SpLookAheadAdapter, which will set the ctrlchar by performing lookahead on Lexer input

---
+++ src/net/sourcemod/sourcepawn/lexer/SpLookAheadLexerAdapter.java
@@ -0,0 +1,67 @@
+package net.sourcemod.sourcepawn.lexer;
+
+import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.FlexLexer;
+import com.intellij.lexer.Lexer;
+import com.intellij.lexer.LookAheadLexer;
+import com.intellij.psi.tree.IElementType;
+
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.BLOCK_COMMENT;
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.CHARACTER_LITERAL;
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.DOC_COMMENT;
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.LINE_COMMENT;
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.NEW_LINE;
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.NUMBER_LITERAL;
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.PRAGMA_CTRLCHAR;
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.WHITE_SPACE;
+
+public class SpLookAheadLexerAdapter extends LookAheadLexer {
+
+  private final _SpLexer spLexer;
+
+  public SpLookAheadLexerAdapter(FlexAdapter flexAdapter) {
+    super(flexAdapter);
+    FlexLexer flexLexer = flexAdapter.getFlex();
+    if (!(flexLexer instanceof _SpLexer)) {
+      throw new IllegalArgumentException(
+          "This implementation requires that FlexLexer of " +
+              "FlexAdapter be a subclass of SourcePawnLexer");
+    }
+
+    spLexer = (_SpLexer) flexAdapter.getFlex();
+  }
+
+  @Override
+  protected void lookAhead(Lexer baseLexer) {
+    final IElementType type = baseLexer.getTokenType();
+    if (type == PRAGMA_CTRLCHAR) {
+      addToken(type);
+      baseLexer.advance();
+      while (baseLexer.getTokenType() == WHITE_SPACE
+          || baseLexer.getTokenType() == LINE_COMMENT
+          || baseLexer.getTokenType() == BLOCK_COMMENT
+          || baseLexer.getTokenType() == DOC_COMMENT) {
+        addToken(WHITE_SPACE);
+        baseLexer.advance();
+      }
+
+      final IElementType token = baseLexer.getTokenType();
+      addToken(token);
+      baseLexer.advance();
+      if (token == CHARACTER_LITERAL) {
+        spLexer.setEscapeCharacter(spLexer.value());
+      } else if (token == NUMBER_LITERAL) {
+        Number number = spLexer.value();
+        int codePoint = number.intValue();
+        spLexer.setEscapeCharacter((char) codePoint);
+      } else if (token == NEW_LINE) {
+        spLexer.resetEscapeCharacter();
+      } else if (token == null) {
+        spLexer.resetEscapeCharacter();
+      }
+    } else {
+      super.lookAhead(baseLexer);
+    }
+  }
+
+}
