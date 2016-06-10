32675abb0ebc7c7b0e6e2d731300c3e552046abc
Collin Smith <collinsmith70@gmail.com>
2016-06-10 02:32:21 -0700

    Implemented BraceMatcher implementation (does not match carrots until I can figure out how to either create or manage those contextually)

---
+++ src/net/sourcemod/sourcepawn/SpBraceMatcher.java
@@ -0,0 +1,36 @@
+package net.sourcemod.sourcepawn;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static net.sourcemod.sourcepawn.lexer.SpTokenTypes.*;
+
+public class SpBraceMatcher implements PairedBraceMatcher {
+
+  private static final BracePair[] BRACE_PAIRS = new BracePair[] {
+      new BracePair(LBRACE, RBRACE, false),
+      new BracePair(LBRACKET, RBRACKET, false),
+      new BracePair(LPAREN, RPAREN, false)
+  };
+
+  @Override
+  public BracePair[] getPairs() {
+    return BRACE_PAIRS;
+  }
+
+  @Override
+  public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lbraceType,
+                                                 @Nullable IElementType contextType) {
+    return true;
+  }
+
+  @Override
+  public int getCodeConstructStart(PsiFile file, int openingBraceOffset) {
+    return openingBraceOffset;
+  }
+}
