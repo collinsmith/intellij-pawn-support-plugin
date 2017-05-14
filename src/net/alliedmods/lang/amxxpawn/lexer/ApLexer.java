02973f59e5fed68225319da56b3fcd8e62da9f8b
Collin Smith <collinsmith70@gmail.com>
2017-05-14 03:18:34 -0700

    Removed MergingLexerAdapter

--- src/net/alliedmods/lang/amxxpawn/lexer/ApLexer.java
+++ src/net/alliedmods/lang/amxxpawn/lexer/ApLexer.java
@@ -1,16 +1,11 @@
 package net.alliedmods.lang.amxxpawn.lexer;
 
 import com.intellij.lexer.FlexAdapter;
-import com.intellij.lexer.MergingLexerAdapter;
-import com.intellij.psi.tree.TokenSet;
 
-import net.alliedmods.lang.amxxpawn.psi.ElementTypes;
-
-public class ApLexer extends MergingLexerAdapter {
+public class ApLexer extends FlexAdapter {
 
   public ApLexer() {
-    super(new FlexAdapter(new _ApLexer()),
-        TokenSet.orSet(ElementTypes.AMXX_COMMENT_BIT_SET, ElementTypes.AMXX_WHITESPACE_BIT_SET));
+    super(new _ApLexer());
   }
 
 }
